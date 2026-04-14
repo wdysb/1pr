7e1b448f87ab4e4907cb45987f4d9990806dad7b
wdysb <mikdrik03@gmail.com>
2026-04-14 21:07:59 +0300

    Update task03.rs

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,21 @@
+using System;
+
+class Solution
+{
+    public static void Staircase(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            string spaces = new string(' ', n - i);
+            string hashes = new string('#', i);
+            Console.WriteLine(spaces + hashes);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Staircase(n);
+    }
+}
 // https://www.hackerrank.com/challenges/staircase/problem
