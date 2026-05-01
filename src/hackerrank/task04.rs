179f52e978c972d88c84815b0064c7c4fa7bacf9
wdysb <mikdrik03@gmail.com>
2026-05-01 09:32:25 +0300

    Update task04.rs

--- src/hackerrank/task04.rs
+++ src/hackerrank/task04.rs
@@ -1,1 +1,43 @@
-// https://www.hackerrank.com/challenges/grading/problem
+using System;
+using System.Collections.Generic;
+
+class Solution
+{
+    public static List<int> gradingStudents(List<int> grades)
+    {
+        List<int> result = new List<int>();
+
+        foreach (int grade in grades)
+        {
+            if (grade >= 38)
+            {
+                int nextMultipleOf5 = ((grade / 5) + 1) * 5;
+                if (nextMultipleOf5 - grade < 3)
+                {
+                    result.Add(nextMultipleOf5);
+                    continue;
+                }
+            }
+            result.Add(grade);
+        }
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        if (!int.TryParse(Console.ReadLine(), out int n)) return;
+
+        List<int> grades = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            if (int.TryParse(Console.ReadLine(), out int grade))
+            {
+                grades.Add(grade);
+            }
+        }
+
+        List<int> result = gradingStudents(grades);
+        Console.WriteLine(string.Join("\n", result));
+    }
+}
