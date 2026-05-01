cb9118f989a108b9dfc886e78ac60e0f7478a7ac
wdysb <mikdrik03@gmail.com>
2026-05-01 09:45:45 +0300

    Update task06.rs

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -4,22 +4,20 @@ class Solution
 {
     static string kangaroo(int x1, int v1, int x2, int v2)
     {
-        if (v1 <= v2)
-        {
-            return "NO";
-        }
-
-        if ((x2 - x1) % (v1 - v2) == 0)
+        if (v1 > v2 && (x2 - x1) % (v1 - v2) == 0)
         {
             return "YES";
         }
-
         return "NO";
     }
 
     static void Main(string[] args)
     {
-        string[] input = Console.ReadLine().Split(' ');
+        string inputLine = Console.ReadLine();
+        if (string.IsNullOrEmpty(inputLine)) return;
+
+        string[] input = inputLine.Split(' ');
+        
         int x1 = int.Parse(input[0]);
         int v1 = int.Parse(input[1]);
         int x2 = int.Parse(input[2]);
