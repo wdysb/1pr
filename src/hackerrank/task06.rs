747c8b85ca0085e5b4d1c96e5cb1e75cc261afde
wdysb <mikdrik03@gmail.com>
2026-05-01 09:42:58 +0300

    Update task06.rs

--- src/hackerrank/task06.rs
+++ src/hackerrank/task06.rs
@@ -1,1 +1,30 @@
-// https://www.hackerrank.com/challenges/kangaroo/problem
+using System;
+
+class Solution
+{
+    static string kangaroo(int x1, int v1, int x2, int v2)
+    {
+        if (v1 <= v2)
+        {
+            return "NO";
+        }
+
+        if ((x2 - x1) % (v1 - v2) == 0)
+        {
+            return "YES";
+        }
+
+        return "NO";
+    }
+
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int x1 = int.Parse(input[0]);
+        int v1 = int.Parse(input[1]);
+        int x2 = int.Parse(input[2]);
+        int v2 = int.Parse(input[3]);
+
+        Console.WriteLine(kangaroo(x1, v1, x2, v2));
+    }
+}
