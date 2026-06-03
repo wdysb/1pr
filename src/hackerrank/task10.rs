bb071eb05014db821c5f4f28bc192e087fafb06c
wdysb <mikdrik03@gmail.com>
2026-06-03 10:27:21 +0300

    Update task10.rs

--- src/hackerrank/task10.rs
+++ src/hackerrank/task10.rs
@@ -1,1 +1,60 @@
 // https://www.hackerrank.com/challenges/sock-merchant/problem
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    public static int sockMerchant(int n, List<int> ar)
+    {
+        HashSet<int> socks = new HashSet<int>();
+        int pairs = 0;
+
+        foreach (int sock in ar)
+        {
+            if (socks.Contains(sock))
+            {
+                socks.Remove(sock);
+                pairs++;
+            }
+            else
+            {
+                socks.Add(sock);
+            }
+        }
+
+        return pairs;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ar = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arTemp => Convert.ToInt32(arTemp)).ToList();
+
+        int result = Result.sockMerchant(n, ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
+
